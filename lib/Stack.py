e6310864d71fa05811791af8ed4716fd3276745a
toel-kamanda34 <toelokemwa@gmail.com>
2024-09-20 14:31:51 +0300

    completed lab

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,48 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        self.items = items
+        self.limit = limit
+        
 
     def isEmpty(self):
-        pass
+        return len(self.items) ==  0 #if list is empty return true
 
+   #add a new item to the top of the stack
     def push(self, item):
-        pass
-
+        if len(self.items) < self.limit:
+            self.items.append(item)
+        else:
+            print("Stack is full")
+    #last in first out
     def pop(self):
-        pass
+        if self.isEmpty():
+            return None
+        return self.items.pop()
 
+    #return top element without removing it 
     def peek(self):
-        pass
+        if self.isEmpty():
+            return None
+        return self.itmes[-1]
     
+    #return length of items list
     def size(self):
-        pass
+        return len(self.items)
 
+    #check if the stack has reached our limit
     def full(self):
-        pass
+        return len(self.items) >= self.limit
 
+    #looks for an iten and returns how far it is from the top if not foun return -1 
     def search(self, target):
-        pass
+        if target in self.items:
+            return len(self.items) - self.items.index(target) - 1
+        return -1
+
+
+my_stack = Stack([1, 2, 3], limit=5)
+print(my_stack.isEmpty())  
+my_stack.push(4)
+my_stack.push(5)
+my_stack.push(6)  
